b4af176232e2b47895e01f4fe4e224bd851f243c
Omar Said <omarsaid1352@gmail.com>
2024-08-02 13:09:37 +0300

    adding user model

---
+++ come_back/src/main/java/com/cb/come_back/Entity/User.java
@@ -0,0 +1,15 @@
+package com.cb.come_back.Entity;
+
+public class User {
+
+    private Long Id;
+
+    private String userName;
+
+    private String useremail;
+
+    private String userpassword;
+
+    private String userphone;
+
+}
